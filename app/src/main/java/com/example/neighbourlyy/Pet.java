0b6e465cf56b7d5a33da7eb1678b13271bba4eca
224094 Lemiesz Aleksander <224094@edu.p.lodz.pl>
2020-12-18 18:24:58 +0100

    Added functions to pet and user.

--- app/src/main/java/com/example/neighbourlyy/Pet.java
+++ app/src/main/java/com/example/neighbourlyy/Pet.java
@@ -4,12 +4,27 @@ import java.util.Date;
 import java.math.BigDecimal;
 
 public class Pet {
+    private int id;
     private String name;
     private String breed;
     private BigDecimal weight;
     private Date availableFrom;
     private Date availableTo;
     private String details;
+
+    public Pet(int id, String name, String breed, BigDecimal weight, Date availableFrom, Date availableTo, String details) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+        this.weight = weight;
+        this.availableFrom = availableFrom;
+        this.availableTo = availableTo;
+        this.details = details;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public void setAvailableFrom(Date availableFrom) {
         this.availableFrom = availableFrom;
@@ -20,15 +35,6 @@ public class Pet {
     }
 
     public void setDetails(String details) {
-        this.details = details;
-    }
-
-    public Pet(String name, String breed, BigDecimal weight, Date availableFrom, Date availableTo, String details) {
-        this.name = name;
-        this.breed = breed;
-        this.weight = weight;
-        this.availableFrom = availableFrom;
-        this.availableTo = availableTo;
         this.details = details;
     }
 
--- app/src/main/java/com/example/neighbourlyy/User.java
+++ app/src/main/java/com/example/neighbourlyy/User.java
@@ -12,6 +12,15 @@ public class User {
     private String password;
     private List<Pet> pets = new ArrayList<>();
 
+    public User(int id, String fullName, String email, String phoneNum, String address, String password) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.address = address;
+        this.password = password;
+    }
+
     public String getFullName() {
         return fullName;
     }
@@ -30,5 +39,26 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public boolean addPet(Pet pet) {
+        return pets.add(pet);
+    }
+
+    public boolean removePet(Pet pet) {
+        return pets.remove(pet);
+    }
+
+    public Pet getPetById(int id) {
+        for (Pet pet : pets) {
+            if (pet.getId() == id) {
+                return pet;
+            }
+        }
+        return null;
+    }
+
+    public boolean removePetById(int id) {
+        return pets.remove(getPetById(id));
     }
 }
