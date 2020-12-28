414d5324b63ee478786f7fa418eda53501c07c0d
Kuba Jaruga <kubajaru@gmail.com>
2020-12-28 11:16:04 +0100

    Starting with authentication

--- app/src/main/java/com/example/neighbourlyy/CreateAccount.java
+++ app/src/main/java/com/example/neighbourlyy/CreateAccount.java
@@ -3,12 +3,23 @@ package com.example.neighbourlyy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccount extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void createAccount(String email, String password) {
+        boolean validEmail = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+
     }
 }
