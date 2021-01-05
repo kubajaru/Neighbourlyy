cb076e55be4aac564b4148032d849b00be73a72d
Kuba Jaruga <kubajaru@gmail.com>
2021-01-05 20:46:17 +0100

    Changing approach to create account. Remove Geocoder.

---
+++ app/src/main/java/com/example/neighbourlyy/User.java
@@ -0,0 +1,9 @@
+package com.example.neighbourlyy;
+
+public class User {
+    public String name;
+    public String phoneNumber;
+    public String postalCode;
+    public String city;
+    public String street;
+}
