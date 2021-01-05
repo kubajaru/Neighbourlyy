80cd8eaf694b1c3ca75a3067436565cf7b1bd158
Kuba Jaruga <kubajaru@gmail.com>
2021-01-05 20:51:00 +0100

    Started working on settings

--- app/src/main/java/com/example/neighbourlyy/Settings.java
+++ app/src/main/java/com/example/neighbourlyy/Settings.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Settings extends AppCompatActivity {
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,10 @@ public class Settings extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("users/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        EditText phoneNumberTV = findViewById(R.id.ETPhoneEdit);
     }
 }
